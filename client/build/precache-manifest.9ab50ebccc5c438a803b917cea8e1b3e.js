66905957e61e5e6667b83a42f2818f8ac394a2e5
Megan Coppins <meganlcoppins@gmail.com>
2020-06-02 20:42:49 -0700

    updated auth0 info

---
+++ client/build/precache-manifest.9ab50ebccc5c438a803b917cea8e1b3e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89f89c55fa547ba76bc4be512a49880d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0bd82c90b79d011db5a",
+    "url": "/static/css/2.8dbe6964.chunk.css"
+  },
+  {
+    "revision": "dbc2cb5c0ad60e4c2e2c",
+    "url": "/static/css/main.0c8ebf46.chunk.css"
+  },
+  {
+    "revision": "b0bd82c90b79d011db5a",
+    "url": "/static/js/2.854e0770.chunk.js"
+  },
+  {
+    "revision": "db8b5a233582a9387c16ea0f82ec90fc",
+    "url": "/static/js/2.854e0770.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbc2cb5c0ad60e4c2e2c",
+    "url": "/static/js/main.1f35b9ae.chunk.js"
+  },
+  {
+    "revision": "81a3ffae95bb6494388b",
+    "url": "/static/js/runtime-main.32a54a46.js"
+  },
+  {
+    "revision": "364a76291709edfb05ce8e882297c60f",
+    "url": "/static/media/logo6.364a7629.png"
+  },
+  {
+    "revision": "4493851dbd1330034474d7261af7efaa",
+    "url": "/static/media/logo7.4493851d.png"
+  }
+]);
